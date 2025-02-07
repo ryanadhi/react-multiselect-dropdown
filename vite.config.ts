2f06138e6a93ddce667ca705ab840279f011ad51
ryanadhi <ryanadhi.p@gmail.com>
2025-02-07 16:10:29 +0700

    feat: differentiate between web and package

--- vite.config.ts
+++ vite.config.ts
@@ -3,34 +3,36 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import dts from "vite-plugin-dts";
 
-const isPackageBuild = process.env.BUILD_MODE === "package";
+export default defineConfig((mode) => {
+  const isPackageBuild = mode.mode === "development";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    dts({ outDir: "dist", clearPureImport: true, rollupTypes: true }),
-  ],
-  build: {
-    lib: isPackageBuild
-      ? {
-          entry: "src/index.ts",
-          name: "ReactMultiselectDropdown",
-          formats: ["es", "cjs"],
-          fileName: (format) => `index.${format === "es" ? "mjs" : "js"}`,
-        }
-      : undefined,
-    rollupOptions: {
-      external: isPackageBuild ? ["react", "react-dom"] : [],
-      output: {
-        assetFileNames: isPackageBuild
-          ? "index.css"
-          : "assets/[name].[hash].css",
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+  return {
+    plugins: [
+      react(),
+      tailwindcss(),
+      dts({ outDir: "dist", clearPureImport: true, rollupTypes: true }),
+    ],
+    build: {
+      lib: isPackageBuild
+        ? {
+            entry: "src/index.ts",
+            name: "ReactMultiselectDropdown",
+            formats: ["es", "cjs"],
+            fileName: (format) => `index.${format === "es" ? "mjs" : "js"}`,
+          }
+        : undefined,
+      rollupOptions: {
+        external: isPackageBuild ? ["react", "react-dom"] : [],
+        output: {
+          assetFileNames: isPackageBuild
+            ? "index.css"
+            : "assets/[name].[hash].css",
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
         },
       },
     },
-  },
+  };
 });
